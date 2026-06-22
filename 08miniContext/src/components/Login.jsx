75615080a46206b35e001171fda26408b430f811
nv-omkareshwar-dwivedy <omkareshwar.dwivedy@newvision-software.com>
2026-06-22 13:08:10 +0530

    Context API - Part I

---
+++ 08miniContext/src/components/Login.jsx
@@ -0,0 +1,23 @@
+import React, {useState, useContext} from "react";
+import UserContext from "../context/UserContext";
+
+export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {setUser} = useContext(UserContext);
+
+    function submit(e) {
+        e.preventDefault();
+        setUser({username, password});
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="username"/>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password"/>
+            <button onClick={submit}>Submit</button>
+        </div>
+    )
+}
---
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserContext from "./UserContext";
+import { useState } from "react";
+
+export default function UserContextProvider({children}) {
+    const [user, setUser] = useState(null);
+    
+    return (
+        <UserContext.Provider value={{user, setUser}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
