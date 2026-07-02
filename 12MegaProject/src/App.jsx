5bdcb0d11777686e105036eb8363ec79311eccac
nv-omkareshwar-dwivedy <omkareshwar.dwivedy@newvision-software.com>
2026-07-02 18:16:31 +0530

    mega project

---
+++ 12MegaProject/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <h1 className="text-3xl text-center">Blog App with AppWrite</h1>
+    </>
+  )
+}
+
+export default App
